88c709d8f7b2bdbf9e3c683beb3405d870727c8f
rilian <dmitriyis@gmail.com>
2013-09-02 14:56:57 +0300

    cleanup

--- vagrant_scripts/after_script.sh
+++ vagrant_scripts/after_script.sh
@@ -5,8 +5,5 @@ echo 'After script'
 echo 'Installing gems...'
 bundle install --gemfile=/vagrant/Gemfile
 
-echo 'Prepare database'
-cd /vagrant && rake db:create && rake db:migrate && rake db:seed
-
 echo 'Run Rails'
 cd /vagrant && rails s
